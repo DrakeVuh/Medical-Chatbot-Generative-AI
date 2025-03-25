8c31dea3c6095e9d937ff05c87da414797cac451
Drake <drake@MacBook-Pro-cua-Drake.local>
2025-03-25 15:02:01 +0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative Medical AI Chatbot Project',
+    version = '0.0.0',
+    author = 'Drake Vuh',
+    author_email = 'vuhlong.work@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
